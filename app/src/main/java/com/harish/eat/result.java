546e79e85c39ee643fe4eb2c4e5ec80fb17d3fd4
aditya314 <anksshady@gmail.com>
2017-12-02 20:15:25 +0530

    Add two fonts arcade.ttf and pac-font.ttf Make theme blue(#3F51B5) and yellow (#fdff00) Update scoreboard UI by modifying result.java and result.xml

--- app/src/main/java/com/harish/eat/result.java
+++ app/src/main/java/com/harish/eat/result.java
@@ -3,17 +3,35 @@ package com.harish.eat;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class result extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        Typeface pacmanTypeface = Typeface.createFromAsset(getAssets(), "fonts/pac-font.ttf");
+        Typeface arcadeTypeface = Typeface.createFromAsset(getAssets(), "fonts/arcade.ttf");
+
+        TextView gameOverTV = (TextView) findViewById(R.id.gameover);
+        gameOverTV.setTypeface(pacmanTypeface);
+
+        TextView scoreTV = (TextView) findViewById(R.id.scorelabel);
+        scoreTV.setTypeface(arcadeTypeface);
+
+        TextView highScoreTV = (TextView) findViewById(R.id.highScoreLabel);
+        highScoreTV.setTypeface(arcadeTypeface);
+
+        Button tryAgainButton = (Button) findViewById(R.id.tryAgain);
+        tryAgainButton.setTypeface(pacmanTypeface);
 
         TextView scorelabel=(TextView)findViewById(R.id.scorelabel);
         TextView highScoreLabel=(TextView)findViewById(R.id.highScoreLabel);
@@ -33,7 +51,7 @@ public class result extends AppCompatActivity {
         }
         else
         {
-            highScoreLabel.setText("Hgh Score : "+highScore);
+            highScoreLabel.setText("High Score : "+highScore);
         }
     }
 
